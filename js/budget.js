b34fc5dde3c341c3bb4958061cd16c450cc790e1
Subahan <subahanislam19@gmail.com>
2022-11-09 21:46:32 +0600

    js code added

---
+++ js/budget.js
@@ -0,0 +1,80 @@
+// first row start
+document.getElementById('select').addEventListener('click',function(){
+    var parent = document.getElementById('add')
+    var li = document.createElement('li')
+    li.innerText = 'Lionel Messi';
+    parent.appendChild(li)
+})
+document.getElementById('selectone').addEventListener('click',function(){
+    var parent = document.getElementById('add')
+    var li = document.createElement('li')
+    li.innerText = 'Neymar Jr';
+    parent.appendChild(li)
+})
+document.getElementById('selecttwo').addEventListener('click',function(){
+    var parent = document.getElementById('add')
+    var li = document.createElement('li')
+    li.innerText = 'Kylian Mbappé';
+    parent.appendChild(li)
+})
+// first row end
+// second row start
+document.getElementById('selectthree').addEventListener('click',function(){
+    var parent = document.getElementById('add')
+    var li = document.createElement('li')
+    li.innerText = 'Vítor Machado';
+    parent.appendChild(li)
+})
+document.getElementById('selectfour').addEventListener('click',function(){
+    var parent = document.getElementById('add')
+    var li = document.createElement('li')
+    li.innerText = 'Sergio Ramos';
+    parent.appendChild(li)
+})
+document.getElementById('selectfive').addEventListener('click',function(){
+    var parent = document.getElementById('add')
+    var li = document.createElement('li')
+    li.innerText = 'Renato Sanches';
+    parent.appendChild(li)
+})
+// second row end
+// third row start
+document.getElementById('selectsix').addEventListener('click',function(){
+    var parent = document.getElementById('add')
+    var li = document.createElement('li')
+    li.innerText = 'Runando';
+    parent.appendChild(li)
+})
+document.getElementById('selectseven').addEventListener('click',function(){
+    var parent = document.getElementById('add')
+    var li = document.createElement('li')
+    li.innerText = 'Kotinho';
+    parent.appendChild(li)
+})
+document.getElementById('selecteight').addEventListener('click',function(){
+    var parent = document.getElementById('add')
+    var li = document.createElement('li')
+    li.innerText = 'Recherlichon';
+    parent.appendChild(li)
+})
+//  third row end
+// four row start
+document.getElementById('selectnine').addEventListener('click',function(){
+    var parent = document.getElementById('add')
+    var li = document.createElement('li')
+    li.innerText = 'Jesos';
+    parent.appendChild(li)
+})
+document.getElementById('selectten').addEventListener('click',function(){
+    var parent = document.getElementById('add')
+    var li = document.createElement('li')
+    li.innerText = 'Dimariya';
+    parent.appendChild(li)
+})
+document.getElementById('selectelevent').addEventListener('click',function(){
+    var parent = document.getElementById('add')
+    var li = document.createElement('li')
+    li.innerText = 'Naymer Jr';
+    parent.appendChild(li)
+})
+// third row end
---
+++ js/count.js
@@ -0,0 +1,21 @@
+document.getElementById('calculate').addEventListener('click',function(){
+    var inputField = document.getElementById('inputfield')
+    var inputvalue = parseFloat(inputField.value)
+   var outputvalue = document.getElementById('expensive')
+   outputvalue.innerText = inputvalue*5;
+    inputField.value = ' ' ;
+
+    // coach and manager expensive count
+    document.getElementById('final-calculate').addEventListener('click',function(){
+        var inputField = document.getElementById('manager')
+        var inputvalue = parseFloat(inputField.value)
+        inputField.value = ' ' ;
+    
+        var inputField = document.getElementById('coach')
+        var inputvalues = parseFloat(inputField.value)
+        inputField.value = ' ' ;
+        var newvalue = inputvalue + inputvalues + outputvalue.innerText;
+        var finalamount = document.getElementById('total')
+        finalamount.innerText = newvalue;
+    })
+})
